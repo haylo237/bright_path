f9cfe379b3cd7d5e90a922534c0ad1b033ef0969
Njike Njiah Abednego <njikeabednego@hotmail.com>
2025-01-26 16:51:32 +0100

    authentication done

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { role } from "@/lib/data";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -117,7 +117,9 @@ const menuItems = [
   },
 ];
 
-const Menu = () => {
+const Menu = async () => {
+  const user = await currentUser();
+  const role = user?.publicMetadata.role as string;
   return (
     <div className="mt-4 text-sm">
       {menuItems.map((i) => (
@@ -144,5 +146,6 @@ const Menu = () => {
     </div>
   );
 };
+
 
 export default Menu;
