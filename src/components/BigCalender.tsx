b45ba7580b15d3a863f76c19170316f6f271f526
Njike Njiah Abednego <njikeabednego@hotmail.com>
2025-01-27 00:48:43 +0100

    progress on schedule

--- src/components/BigCalender.tsx
+++ src/components/BigCalender.tsx
@@ -2,13 +2,17 @@
 
 import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
 import moment from "moment";
-import { calendarEvents } from "@/lib/data";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useState } from "react";
+import { adjustScheduleToCurrentWeek } from "@/lib/utils";
 
 const localizer = momentLocalizer(moment);
 
-const BigCalendar = () => {
+const BigCalendar = ({
+  data,
+}: {
+  data: { title: string; start: Date; end: Date }[];
+}) => {
   const [view, setView] = useState<View>(Views.WORK_WEEK);
 
   const handleOnChangeView = (selectedView: View) => {
@@ -18,15 +22,15 @@ const BigCalendar = () => {
   return (
     <Calendar
       localizer={localizer}
-      events={calendarEvents}
+      events={data}
       startAccessor="start"
       endAccessor="end"
       views={["work_week", "day"]}
       view={view}
       style={{ height: "98%" }}
       onView={handleOnChangeView}
-      min={new Date(2025, 1, 0, 8, 0, 0)}
-      max={new Date(2025, 1, 0, 17, 0, 0)}
+      min={new Date(2025, 1, 0, 7, 30, 0)}
+      max={new Date(2026, 1, 0, 16, 0, 0)}
     />
   );
 };
