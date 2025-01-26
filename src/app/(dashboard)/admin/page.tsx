ba86eff960502b5393b9dafbbf7024b66ddfb32a
Njike Njiah Abednego <njikeabednego@hotmail.com>
2025-01-26 22:22:12 +0100

    CRUD operations for events and announcements

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,11 +1,12 @@
 import Announcements from "@/components/Announcements";
-import AttendanceChart from "@/components/AttendanceChart";
-import CountChart from "@/components/CountChart";
+import AttendanceChartContainer from "@/components/AttendanceChartContainer";
 import EventCalendar from "@/components/EventCalendar";
+import EventCalendarContainer from "@/components/EventCalendarContainer";
 import FinanceChart from "@/components/FinanceChart";
+import CountChartContainer from "@/components/forms/CountChartContainer";
 import UserCard from "@/components/UserCard";
 
-const AdminPage = () => {
+const AdminPage = ({searchParams}:{searchParams: {[keys:string]:string | undefined}}) => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
@@ -21,11 +22,11 @@ const AdminPage = () => {
         <div className="flex gap-4 flex-col lg:flex-row">
           {/* COUNT CHART */}
           <div className="w-full lg:w-1/3 h-[450px]">
-            <CountChart />
+            <CountChartContainer />
           </div>
           {/* ATTENDANCE CHART */}
           <div className="w-full lg:w-2/3 h-[450px]">
-            <AttendanceChart />
+            <AttendanceChartContainer/>
           </div>
         </div>
         {/* BOTTOM CHART */}
@@ -35,7 +36,7 @@ const AdminPage = () => {
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
+        <EventCalendarContainer searchParams={searchParams}/>
         <Announcements/>
       </div>
     </div>
